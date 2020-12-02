03c1744622de09ca7d90390d355d4ac145ee850a
Daniel Vargas <davarg5@yahoo.com>
2020-12-01 19:27:08 -0500

    finished mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -73,11 +73,41 @@ router.get('/:id/comments', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-    
+    const { id } = req.params;
+    Post.remove(id)
+        .then(posts => {
+            if(!posts) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+            res.status(202).json(posts);
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The post could not be removed" });
+        })
 })
 
 router.put('/:id', (req, res) => {
-    
+    if(!req.body.title || !req.body.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+    const { id }  = req.params;
+    Post.update(id, req.body)
+        .then(updatedRecords => {
+            console.log(updatedRecords);
+            if(updatedRecords > 0) {
+                Post.findCommentById(id)
+                    .then(post => {
+                        res.status(200).json(post);
+                    })
+            }
+            else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        
+        .catch(() => {
+            res.status(500).json({ error: "The post information could not be modified." })
+        })
 })
 
 module.exports = router;
